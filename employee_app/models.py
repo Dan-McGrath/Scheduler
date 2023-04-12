95ce519b563a2b033ddeaca3b9abe6f5098dbe39
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-04-12 10:00:46 -0400

    Cleaned up code

--- employee_app/models.py
+++ employee_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from django.urls import reverse
 from django.core.validators import RegexValidator
 # Create your models here.
@@ -6,33 +7,52 @@ from django.core.validators import RegexValidator
 #Employee Model
 
 class Employee(models.Model):
-    SUNDAY = 'SU'
-    MONDAY = 'MO'
-    TUESDAY = 'TU'
-    WEDNESDAY = 'WE'
-    THURSDAY = 'TH'
-    FRIDAY = 'FR'
-    SATURDAY = 'SA'
-    DAYS_OF_WEEK = [
-        (SUNDAY, 'Sunday'),
-        (MONDAY, 'Monday'),
-        (TUESDAY, 'Tuesday'),
-        (WEDNESDAY, 'Wednesday'),
-        (THURSDAY, 'Thursday'),
-        (FRIDAY, 'Friday'),
-        (SATURDAY, 'Sunday')
+#     SUNDAY = 'SU'
+#     MONDAY = 'MO'
+#     TUESDAY = 'TU'
+#     WEDNESDAY = 'WE'
+#     THURSDAY = 'TH'
+#     FRIDAY = 'FR'
+#     SATURDAY = 'SA'
+#     DAYS_OF_WEEK = [
+#         (SUNDAY, 'Sunday'),
+#         (MONDAY, 'Monday'),
+#         (TUESDAY, 'Tuesday'),
+#         (WEDNESDAY, 'Wednesday'),
+#         (THURSDAY, 'Thursday'),
+#         (FRIDAY, 'Friday'),
+#         (SATURDAY, 'Sunday')
+#     ]
+
+    MORNING = 'MO'
+    AFTERNOON = 'AF'
+    MID = 'MI'
+    NIGHT_AUDIT = 'NA'
+    PREFERED_SHIFT_CHOICES = [
+        (MORNING, 'Morning'),
+        (MID, 'Mid/Split'),
+        (AFTERNOON, 'Afternoon'),
+        (NIGHT_AUDIT, 'Night Audit')
     ]
+
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     phone_regex = RegexValidator(regex=r'^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})')
     phone_number = models.CharField(validators=[phone_regex], max_length=17, unique=True) # Validators should be a list
-    hire_date = models.DateField()
-    pto = models.IntegerField(default=0)
-    sick_days = models.IntegerField(default=0)
-    hourly_pay = models.DecimalField(max_digits=6, decimal_places=2)
-    
+    paid_time_off = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    sick_days = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    hours_worked = models.DecimalField(default=0, max_digits=4, decimal_places=2)
+    prefered_shift = models.CharField(
+        max_length=2,
+        choices=PREFERED_SHIFT_CHOICES,
+        default=MORNING,
+        null=True,
+    )
+
+    #hourly_pay = models.DecimalField(max_digits=6, decimal_places=2)
+    #hire_date = models.DateField(editable=False, blank=True)
 
     class Meta:
         verbose_name = ("Employee")
--- employee_app/views.py
+++ employee_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import ListView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
+from .models import Employee
 # Create your views here.
+
+class EmployeeHome(ListView):
+    model = Employee
+    template_name = 'employee_app/employee_home.html'
+    
+    
